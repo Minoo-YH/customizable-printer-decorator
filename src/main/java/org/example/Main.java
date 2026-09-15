cfa8a22d066735dbd2d2688affb3c654c9921e43
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-09-15 23:24:57 +0300

    Add Main demonstration

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Printer printer = new BasicPrinter();
+        printer.print("Hello World!");
+
+        Printer printer2 =
+                new EncryptedPrinter(
+                        new XMLPrinter(
+                                new BasicPrinter()
+                        )
+                );
+
+        printer2.print("Hello World!");
+    }
+}
